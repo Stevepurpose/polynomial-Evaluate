0dfd044664f86ef60a2ba5f477457b8f262d2a75
Stevepurpose <stephenodogwu83@gmail.com>
2023-10-10 22:59:16 +0200

    made my function recursive

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 let polynomial=[]
-    
 var y=polynomial.length
 var tosub=document.getElementById("tosub")
 let topushA=document.getElementById("topush")
@@ -7,8 +6,6 @@ let choices=document.querySelector(".choices")
 let coefficients=topushA.value
 var button=document.querySelector("button")
 let toread=document.getElementById("toread")
-
-
 
 /*event delegation*/
 choices.addEventListener("click",clicked)
@@ -20,8 +17,8 @@ switch(button){
   /*enter x coefficients*/
 case but1:
  coefficients=topushA.value
-toread.innerHTML=coefficients+ " " + "is added "
-polynomial.push(+coefficients)
+toread.innerHTML=coefficients + " " + "is added "
+polynomial.push(+coefficients)  //+ to convert string to number
 break;
 
 /*display*/
@@ -29,33 +26,29 @@ case but2:
 n=polynomial.length
 coefficients =polynomial[i]
 toread.innerHTML="coefficients of powers of x are:<br/>"
-for(var i=0;i<n;i++){
-    
+for(var i=0;i<n;i++){   
 toread.innerHTML+=polynomial[i] + " " 
 }
 
 break;
 
-
 /*sum polynomial*/
 case but3: 
-var unknown=tosub.value
 //applying principle of Horner's algorithm  
-function Horner(polynomial,y,unknown){
+function Horner(polynomial){
 y=polynomial.length
-//initialize result with first coefficient of x at array index 0
-var result=polynomial[0]
-for( var i=1;i<polynomial.length;i++){
+if(i==y) return;
 result=polynomial[i]+unknown*result
+i++
+Horner(polynomial) 
 }
-return result
-
-}
-toread.innerHTML="The sum of the polynomial is:"+
-Horner(polynomial,y,unknown) 
+let unknown=tosub.value
+var i=1
+//initialize result with first coefficient of x at array index 0
+let result=polynomial[0]
+Horner(polynomial) 
+toread.innerHTML="The sum of the polynomial is:"+ result
 break;
-
-
 }
 }
 }
